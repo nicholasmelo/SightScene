dc1aa248a7fb09ab517dadbe55c0d2df90996b97
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-10-04 16:29:30 -0600

    Search locates movie by title and sets MovieID to be used for another search.

--- assets/JS/getID.js
+++ assets/JS/getID.js
@@ -25,12 +25,17 @@ fetch(url, {
     return response.json();
   })
   .then(data => {
-    console.log('Response Data:', data); // Log the response data
+    console.log('API Response Data:', data);
+    if(data && Array.isArray(data.results) && data.results.length >= 0) {
+      let movieID =data.results[0,0].imdb_id;
+      console.log('MovieID: ', movieID);
+    } else {
+      console.log('No Movie ID found for', movieName)
+    }
   })
   .catch(error => {
     console.error('Fetch Error:', error);
   });
-}
+};
 
 searchBtn.addEventListener('click', getMovieID);
-console.log(getMovieID());
