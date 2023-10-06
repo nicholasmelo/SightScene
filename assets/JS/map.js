05e5a18335fddf4e7166f07f322d4ead545a19cb
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-10-06 11:18:55 -0400

    in-progress merge of example code

--- assets/JS/map.js
+++ assets/JS/map.js
@@ -3,7 +3,7 @@ let map;
 let directionsService = new google.maps.DirectionsService();
 let directionsRenderer;
 // Create a single array to hold all infoWindows
-let infoWindows = [];
+let markers = [];
 
 //function for building out map with film location data onto page *name from googleAPI url*
 function initMap() {
@@ -241,32 +241,147 @@ function initMap() {
   //replace with data from miniMoviesAPI *DD Coordinate Form*
   //e.g. The Sandlot Movie
   var locations = [
-    ["Smith Ballpark", 40.7377753822, -111.889054777],
-    ["Lagoon", 40.984444, -111.895],
-    ["George S. Eccles Dinosaur Park", 41.23773, -111.9379],
-    ["Tracy Aviary", 40.74258, -111.87489],
-    ["Salt Lake City", 40.7608, -111.8911],
+    {
+      position: { lat: 40.7377753822, lng: -111.889054777 },
+      title: "Smith Ballpark",
+    },
+    {
+      position: { lat: 40.984444, lng: -111.895 },
+      title: "Lagoon",
+    },
+    {
+      position: { lat: 41.23773, lng: -111.9379 },
+      title: "George S. Eccles Dinosaur Park",
+    },
+    {
+      position: { lat: 40.74258, lng: -111.87489 },
+      title: "Tracy Aviary",
+    },
+    {
+      position: { lat: 40.7608, lng: -111.8911 },
+      title: "Salt Lake City",
+    },
   ];
 
-  for (var i = 0; i < locations.length; ++i) {
-    // Create a link element
-    var link = document.createElement("a");
-    link.href = "#";
-    link.textContent = "GET DIRECTIONS";
-
-    // Add a click event listener to the link using an IIFE
-    (function (index) {
-      link.addEventListener("click", function (event) {
-        event.preventDefault();
-        GetDirections(locations[index][1], locations[index][2]);
-        console.log("link was clicked for location " + index);
-      });
-    })(i);
-
-    // Create the InfoWindow with the location label and the link
-    var infoWindow = new google.maps.InfoWindow({
-      content: locations[i][0] + "<br>" + link.outerHTML,
+  // Create an info window to share between markers.
+  const infoWindow = new InfoWindow();
+  // Create the markers.
+  // tourStops.forEach(({ position, title, }, i) => {
+  for (let i = 0; i < locations.length; i++) {
+    const pin = new PinElement({
+      glyph: `${i + 1}`,
     });
+    let position = locations[i].position;
+    let title = locations[i].title;
+    const marker = new AdvancedMarkerElement({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      content: pin.element,
+    });
+    //CREATE DIRECTIONS LINK USING FOR LOOP TO ASSIGN THE LAT & LNG TO EACH CONTENT WINDOW
+    let infoWindowContent = "";
+    let directionsLink = "";
+
+    directionsLink = document.createElement("p");
+    directionsLink.textContent = "Get Directions";
+    directionsLink.style = "color: blue; text-decoration: underline";
+
+    //SET THE LAT & LNG FOR EACH END LOCATION AS A DATA ATTRIBUTE
+    directionsLink.setAttribute("data-destLat", i); //setting dummy values for the lat
+    directionsLink.setAttribute("data-destLng", i + 10); //setting dummy values for the lng
+
+    //SET THE CONTENT IN THE INFO WINDOW TO BE EQUAL TO THE DIRECTIONS LINK
+    infoWindowContent = directionsLink;
+
+    //ADD AN EVENT LISTENER TO THE DIRECTIONS LINK
+    directionsLink.addEventListener("click", function (event) {
+      console.log("click on directions link");
+
+      // console.log(event);
+      console.log(event.target);
+
+      //GET CURRENT LOCATION
+      let originLocationLat = 0;
+      let originLocationLng = 0;
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+        originLocationLat = position.coords.latitude;
+        originLocationLng = position.coords.longitude;
+      });
+      console.log(originLocationLat);
+      console.log(originLocationLng);
+
+      //GET THE END LAT AND LNG FOR THE MARKER / END LOCATION
+      let destLat = event.target.getAttribute("data-destLat");
+      let destLng = event.target.getAttribute("data-destLng");
+      console.log("data attibute destLat= " + destLat);
+      console.log("data attibute destLng= " + destLng);
+    });
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", ({ domEvent, latLng }) => {
+      //parameters here might not be necessary
+      const { target } = domEvent; //might not be necessary
+      // console.log(target);
+      // console.log(latLng);
+      infoWindow.close();
+      infoWindow.setContent(infoWindowContent);
+
+      // infoWindow.setContent(marker.title); //original code
+      infoWindow.open(marker.map, marker);
+      console.log("click on marker");
+
+      //calculate route to selected location
+      function GetDirections(destLat, destLng) {
+        var origin = { lat: 40.7608, lng: -111.8911 };
+        //var destination = { lat: 41.23773, lng: -111.9379 };
+
+        var request = {
+          origin: new google.maps.LatLng(origin.lat, origin.lng),
+          destination: new google.maps.LatLng(destLat, destLng),
+          travelMode: "DRIVING", // or 'WALKING', 'BICYCLING', etc.
+        };
+
+        directionsService.route(request, function (result, status) {
+          if (status == "OK") {
+            // Display the route on the map
+            directionsRenderer.setDirections(result);
+          } else {
+            console.error("Directions request failed due to " + status);
+          }
+        });
+        directionsRenderer.setPanel(document.getElementById("info"));
+      }
+      //call getDirections function
+      GetDirections();
+    });
+  }
+}
+
+// calling function to generate map on page
+initMap();
+
+//-------------------------------------------------------------------------------------//
+// Create the InfoWindow with the location label and the link
+/*   var infoWindow = new google.maps.InfoWindow({
+      content: locations[i][0] + "<br>" + link.outerHTML,
+    }); */
+
+/* 
+    for (var i = 0; i < locations.length; ++i) {
+      // Create a link element
+      var link = document.createElement("a");
+      link.href = "#";
+      link.textContent = "GET DIRECTIONS";
+  
+      // Add a click event listener to the link
+      link.addEventListener("click", function (event) {
+          event.preventDefault();
+          GetDirections(locations[index][1], locations[index][2]);
+          console.log("link was clicked for location " + index);
+        });
+      };
+  
 
     // Create a marker for the location
     var marker = new google.maps.Marker({
@@ -274,40 +389,5 @@ function initMap() {
       map: map,
     });
 
-    // Associate the marker with the infoWindow
-    marker.addListener("click", function () {
-      // Close any open infoWindows
-      infoWindows.forEach(function (iw) {
-        iw.close();
-      });
-
-      // Open the current infoWindow
-      infoWindow.open(marker.map, marker);
-    });
-
-    // Push the current InfoWindow to the array
-    infoWindows.push(infoWindow);
   }
 }
-function GetDirections(destLat, destLng) {
-  var origin = { lat: 40.7608, lng: -111.8911 };
-
-  var request = {
-    origin: new google.maps.LatLng(origin.lat, origin.lng),
-    destination: new google.maps.LatLng(destLat, destLng),
-    travelMode: "DRIVING", // or 'WALKING', 'BICYCLING', etc.
-  };
-
-  directionsService.route(request, function (result, status) {
-    if (status == "OK") {
-      // Display the route on the map
-      directionsRenderer.setDirections(result);
-    } else {
-      console.error("Directions request failed due to " + status);
-    }
-  });
-  directionsRenderer.setPanel(document.getElementById("info"));
-}
-
-// calling function to generate map on page
-initMap();
