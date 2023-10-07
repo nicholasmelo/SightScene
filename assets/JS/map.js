e899453118a0c405084af9b80f28fb3771cd2246
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-10-07 16:43:35 -0400

    restructure, commenting, and changed lat/lng to city/state

--- assets/JS/map.js
+++ assets/JS/map.js
@@ -7,6 +7,29 @@ let directionsRenderer;
 let markers = [];
 let infoWindows = [];
 
+//calculate route to selected location
+function GetDirections() {
+  //orign should = originLocation (city/state)
+  var origin = "moab, ut";
+  var destination = "salt lake city, ut";
+
+  var request = {
+    origin: origin,
+    destination: destination,
+    travelMode: "DRIVING", // or 'WALKING', 'BICYCLING', etc.
+  };
+
+  directionsService.route(request, function (result, status) {
+    if (status == "OK") {
+      // Display the route on the map
+      directionsRenderer.setDirections(result);
+    } else {
+      console.error("Directions request failed due to " + status);
+    }
+  });
+  directionsRenderer.setPanel(document.getElementById("info"));
+}
+
 //function for building out map with film location data onto page *name from googleAPI url*
 async function initMap() {
   // Request needed libraries.
@@ -56,8 +79,7 @@ async function initMap() {
   // Create an info window to share between markers.
   var infoWindow = new google.maps.InfoWindow();
 
-  // Create the markers.
-  // tourStops.forEach(({ position, title, }, i) => {
+  // Create the markers
   for (let i = 0; i < locations.length; i++) {
     const pin = new PinElement({
       glyph: `${i + 1}`,
@@ -70,302 +92,47 @@ async function initMap() {
       title: `${i + 1}. ${title}`,
       content: pin.element,
     });
-    //CREATE DIRECTIONS LINK USING FOR LOOP TO ASSIGN THE LAT & LNG TO EACH CONTENT WINDOW
+    //initialize infoWindow content and link
     let infoWindowContent = "";
     let directionsLink = "";
 
+    //create element for infoWindow and directions link
     directionsLink = document.createElement("p");
     directionsLink.textContent = "Get Directions";
     directionsLink.style = "color: blue; text-decoration: underline";
 
-    //SET THE LAT & LNG FOR EACH END LOCATION AS A DATA ATTRIBUTE
-    directionsLink.setAttribute("data-destLat", i); //setting dummy values for the lat
-    directionsLink.setAttribute("data-destLng", i + 10); //setting dummy values for the lng
-
-    //SET THE CONTENT IN THE INFO WINDOW TO BE EQUAL TO THE DIRECTIONS LINK
+    //set infoWindow content to the directionsLink content string
     infoWindowContent = directionsLink;
 
-    //ADD AN EVENT LISTENER TO THE DIRECTIONS LINK
+    //EventListener for directions link
     directionsLink.addEventListener("click", function (event) {
-      console.log("click on directions link");
+      console.log("directions link clicked");
 
-      //GET CURRENT LOCATION
-      let originLocationLat = 0;
-      let originLocationLng = 0;
+      //Get current location from geocoder API in city/state form
+      let originLocation;
+      //let originLocationLat = 0;
+      //let originLocationLng = 0;
       navigator.geolocation.getCurrentPosition((position) => {
         console.log(position.coords.latitude, position.coords.longitude);
         originLocationLat = position.coords.latitude;
         originLocationLng = position.coords.longitude;
-      });
-      console.log(originLocationLat);
-      console.log(originLocationLng);
 
-      //GET THE END LAT AND LNG FOR THE MARKER / END LOCATION
-      let destLat = event.target.getAttribute("data-destLat");
-      let destLng = event.target.getAttribute("data-destLng");
-      console.log("data attibute destLat= " + destLat);
-      console.log("data attibute destLng= " + destLng);
+        //MUST BE IN CITY/STATE
+        originLocation = "moab, ut";
+        console.log("your current location is " + originLocation);
+      });
+
+      //calculate directions after link clicked
+      GetDirections();
     });
+
     // Add a click listener for each marker, and set up the info window.
     marker.addListener("click", ({ domEvent, latLng }) => {
-      //parameters here might not be necessary
       const { target } = domEvent; //might not be necessary
-      // console.log(target);
-      // console.log(latLng);
       infoWindow.close();
       infoWindow.setContent(infoWindowContent);
-
-      // infoWindow.setContent(marker.title); //original code
       infoWindow.open(marker.map, marker);
-      console.log("click on marker");
-
-      // Add the directionsRenderer to the map---------> THROWING AN ISSUE HERE!!!!!!!
-      //directionsRenderer.setMap(map);
-
-      //calculate route to selected location
-      function GetDirections(destLat, destLng) {
-        var origin = "los angeles, ca";
-        var destination = "sacramento, ca";
-
-        var request = {
-          origin: origin,
-          destination: destination,
-          travelMode: "DRIVING", // or 'WALKING', 'BICYCLING', etc.
-        };
-
-        directionsService.route(request, function (result, status) {
-          if (status == "OK") {
-            // Display the route on the map
-            directionsRenderer.setDirections(result);
-          } else {
-            console.error("Directions request failed due to " + status);
-          }
-        });
-        directionsRenderer.setPanel(document.getElementById("info"));
-      }
-      //call getDirections function
-      GetDirections();
+      console.log("location selected");
     });
   }
 }
-
-//-------------------------------------------------------------------------------------//
-// old styling- NOT COMPATIBLE WITH MARKERS
-/*styles: [
-  {
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#ebe3cd",
-      },
-    ],
-  },
-  {
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#523735",
-      },
-    ],
-  },
-  {
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#f5f1e6",
-      },
-    ],
-  },
-  {
-    featureType: "administrative",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#c9b2a6",
-      },
-    ],
-  },
-  {
-    featureType: "administrative.land_parcel",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#dcd2be",
-      },
-    ],
-  },
-  {
-    featureType: "administrative.land_parcel",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#ae9e90",
-      },
-    ],
-  },
-  {
-    featureType: "landscape.natural",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae",
-      },
-    ],
-  },
-  {
-    featureType: "poi",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae",
-      },
-    ],
-  },
-  {
-    featureType: "poi",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#93817c",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#a5b076",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#447530",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#f5f1e6",
-      },
-    ],
-  },
-  {
-    featureType: "road.arterial",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#fdfcf8",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#f8c967",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#e9bc62",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway.controlled_access",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#e98d58",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway.controlled_access",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#db8555",
-      },
-    ],
-  },
-  {
-    featureType: "road.local",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#806b63",
-      },
-    ],
-  },
-  {
-    featureType: "transit.line",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae",
-      },
-    ],
-  },
-  {
-    featureType: "transit.line",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#8f7d77",
-      },
-    ],
-  },
-  {
-    featureType: "transit.line",
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#ebe3cd",
-      },
-    ],
-  },
-  {
-    featureType: "transit.station",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#a3c2ae",
-      },
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#92998d",
-      },
-    ],
-  },
-],*/
