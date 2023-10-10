90685199cfa0829bdd70dd1397f1bae53cdfbe5a
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-10-09 19:53:42 -0600

    Updated API merge issues resolved.

--- assets/JS/NewAPI.js
+++ assets/JS/NewAPI.js
@@ -63,7 +63,7 @@ let savedLocationDetails = [];
 function getMovie() {
   movieName = searchBar.value;
   
-  const url = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${movieName}?titleType=movie`;
+  const url = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${movieName}?titleType=movie`; //UPDATED API LINK
 
   console.log('Request URL:', url); // Log the request URL
 
@@ -71,8 +71,8 @@ function getMovie() {
   fetch(url, {
     method: 'GET',
     headers: {
-		'X-RapidAPI-Key': '66c1ef1f94msh8651ee6ecab8edbp195f28jsn7054b5e3cfe0',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		'X-RapidAPI-Key': '66c1ef1f94msh8651ee6ecab8edbp195f28jsn7054b5e3cfe0', //UPDATED API KEY
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com' //UPDATED 
     }
   })
   .then(response => {
@@ -86,8 +86,8 @@ function getMovie() {
     console.log('Request URL:', url); // Log the request URL
     console.log('API Response Data:', data);
     if (data && Array.isArray(data.results) && data.results.length >= 0) {
-      let movieID = data.results[0].id;
-      let movieTitle = data.results[0].originalTitleText.text; //Gets movie title info from the fetch
+      let movieID = data.results[0].id; //UPDATED
+      let movieTitle = data.results[0].originalTitleText.text; //Gets movie title info from the fetch UPDATED
       
       console.log('MovieID: ', movieID);
       console.log('Movie Name: ', movieTitle);
@@ -95,14 +95,14 @@ function getMovie() {
       let currentMovie = document.querySelector(".currentMovie");
       currentMovie.textContent = movieTitle; //Sets the searched movies title to the currentMovie H2 in the HTML to display to users what movie they have searched.
 
-      const pUrl = `https://moviesminidatabase.p.rapidapi.com/movie/id/${movieID}/production_locations/`;
+      const pUrl = `https://moviesdatabase.p.rapidapi.com/titles/${movieID}?info=filmingLocations`; //UPDATED
 
         //Second Fetch request to use mini movies ID to request location info
       fetch(pUrl, {
         method: 'GET',
         headers: {
-          'X-RapidAPI-Key': apiKey,
-          'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+            'X-RapidAPI-Key': '66c1ef1f94msh8651ee6ecab8edbp195f28jsn7054b5e3cfe0', //UPDATED
+            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com' //UPDATED
         }
       })
       .then(response => {
@@ -115,54 +115,11 @@ function getMovie() {
         console.log('API Location Response Data:', data);
         
         if (data) {
-          console.log(data.results.locations.locations[0][0]);
-          let movieLo = data.results.locations.locations[0][0];
+          console.log(data.results.filmingLocations.edges[0].node.text);
+          let movieLo = data.results.filmingLocations.edges[0].node.text;
           console.log('Addresses:', movieLo);
-          var addresses = [movieLo]; //Sets the addresses array with the MovieLo info grabbed from the mini movies api
-          var geocoder = new google.maps.Geocoder();
+          var locations = [movieLo]; //Sets the addresses array with the MovieLo info grabbed from the mini movies api
           
-          // Iterate over the array and geocode each address.
-          for (var i = 0; i < addresses.length; i++) {
-            // Capture the address variable in a closure.
-            (function(address) {
-              // Geocode the address.
-              geocoder.geocode({ address: address }, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                  // Get the latitude and longitude of the address.
-                  var latitude = results[0].geometry.location.lat();
-                  var longitude = results[0].geometry.location.lng();
-                  var locationDetails = [
-                    address,
-                    latitude,
-                    (-longitude)
-                  ];
-                  console.log("Location Details Array:", locationDetails);
-                  
-                  //loop through given locations array to generate on map
-                  for (var i = 0; i < locationDetails.length; ++i) {
-                    // adds location label onto markers
-                    console.log("length: ", locationDetails.length);
-                    console.log("i: ", i);
-                    console.log("details: ", locationDetails[i]);
-                    console.log("location details: ", locationDetails[1], "and second: ", locationDetails[2]);
-                    InfoWindow = new google.maps.InfoWindow({
-                      content: locationDetails[i],
-                      position: new google.maps.LatLng(locationDetails[1], locationDetails[2]),
-                      //position: new google.maps.LatLng(40.76078, -111.89105),
-                      map: map,
-                    });
-                    console.log("info window:", infoWindow);
-                  }
-                  
-                  // Display the coordinates on the web page.
-                  console.log("for "+ address + " the corresponding latitude is " + latitude + " and the corresponding longitude is " + longitude + ".");
-                } else {
-                  // Handle the error.
-                  console.log("Geocoding failed:", status);
-                }
-              });
-            })(addresses[i]);
-          } 
           //Setting local storage info here.
           const searchData = {
             savedMovie: movieTitle,
