25235c9f6528698ccfa073a36736b4dcb856ed05
Monkey D. Luffy <zachchamber07@gmail.com>
2023-10-09 18:50:52 -0600

    unecessary changes

--- assets/JS/MASTER.js
+++ assets/JS/MASTER.js
@@ -1,119 +1,78 @@
-const apiKey = '5131599b8amsh5cd7960c965ca44p1c76fcjsnf65b00e1bb62';
-let movieName = '';
-const searchBar = document.querySelector(".searchBar");
-const searchBtn = document.querySelector(".searchBtn");
-let movieLo ='';
+// initializing our map variable to reference in our initMap function
+let map;
+let directionsRenderer = new google.maps.DirectionsRenderer();
+let directionsService = new google.maps.DirectionsService();
+let infoWindow;
 
+// function for building out map with film location data onto page *name from googleAPI url*
+function initMap() {
+  var mapOptions = {
+    zoom: 9,
+    // map renders centered on Salt Lake City, Utah
+    center: { lat: 40.76078, lng: -111.89105 },
+    // customized map display colors
+    styles: [
+      // ... Your map styles ...
+    ],
+  };
 
-function getMovie() {
-  movieName = searchBar.value;
+  // places map on page in the location designated "map" in our HTML based on our mapOptions
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
 
-  const url = `https://moviesminidatabase.p.rapidapi.com/movie/imdb_id/byTitle/${encodeURIComponent(movieName)}/`;
+function addMovieLocationMarker(location, title) {
+  // Create a marker for the movie location
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: title,
+  });
 
-  console.log('Request URL:', url); // Log the request URL
+  // Add an info window with movie information
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<strong>' + title + '</strong><br>' + location,
+  });
 
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': apiKey,
-      'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+  // Show the info window when the marker is clicked
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
+}
+
+function geocodeAddress(address, callback) {
+  // Create a geocoder instance
+  var geocoder = new google.maps.Geocoder();
+
+  // Geocode the address
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      // If geocoding is successful, call the callback function with the location
+      callback(results[0].geometry.location);
+    } else {
+      // Handle geocoding error
+      console.error('Geocoding failed:', status);
+      callback(null); // Pass null as the result in case of an error
     }
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('API Response Data:', data);
-      if (data && Array.isArray(data.results) && data.results.length >= 0) {
-        let movieID = data.results[0].imdb_id;
-        let movieTitle = data.results[0].title; //Gets movie title info from the fetch
-        console.log('MovieID: ', movieID);
-        console.log('Movie Name: ', movieTitle);
-
-        let currentMovie = document.querySelector(".currentMovie");
-        currentMovie.textContent = movieTitle; //Sets the searched movies title to the currentMovie H2 in the HTML to display to users what movie they have searched.
-
-        const pUrl = `https://moviesminidatabase.p.rapidapi.com/movie/id/${movieID}/production_locations/`;
-
-        fetch(pUrl, {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
-          }
-        })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(data => {
-            console.log('API Location Response Data:', data);
-            
-            if (data) {
-              console.log(data.results.locations.locations[0][0]);
-              let movieLo = data.results.locations.locations[0][0];
-
-              console.log('Addresses:', movieLo);
-              var addresses = [movieLo];
-              var geocoder = new google.maps.Geocoder();
-
-// Iterate over the array and geocode each address.
-for (var i = 0; i < addresses.length; i++) {
-  // Capture the address variable in a closure.
-  (function(address) {
-    // Geocode the address.
-    geocoder.geocode({ address: address }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        // Get the latitude and longitude of the address.
-        var latitude = results[0].geometry.location.lat();
-        var longitude = results[0].geometry.location.lng();
-
-        var locationDetails = [
-            address,
-            latitude,
-            (-longitude)
-          ];
-      console.log("Location Details Array:", locationDetails);
-    
-
-  //loop through given locations array to generate on map
-  for (var i = 0; i < locationDetails.length; ++i) {
-    // adds location label onto markers
-    console.log("length: ", locationDetails.length);
-    console.log("i: ", i);
-    console.log("details: ", locationDetails[i]);
-    console.log("location details: ", locationDetails[1], "and second: ", locationDetails[2]);
-    InfoWindow = new google.maps.InfoWindow({
-      content: locationDetails[i],
-      position: new google.maps.LatLng(locationDetails[1], locationDetails[2]),
-      //position: new google.maps.LatLng(40.76078, -111.89105),
-      map: map,
-    });
-    console.log("info window:", infoWindow);
-  }
-
-        // Display the coordinates on the web page.
-        console.log("for "+ address + " the corresponding latitude is " + latitude + " and the corresponding longitude is " + longitude + ".");
-      } else {
-        // Handle the error.
-        console.log("Geocoding failed:", status);
-      }
-    });
-  })(addresses[i]);
-}
-            }
-          })
-      }
-    })
-    .catch(error => {
-      console.error('Fetch Error 1:', error);
-    });
+  });
 }
 
+function getMovieLocation() {
+  // ... Your existing code for fetching movie location data ...
 
-searchBtn.addEventListener('click', getMovie);
+  // Geocode and add a marker for the movie location
+  geocodeAddress(movieLocation, function (location) {
+    if (location) {
+      addMovieLocationMarker(location, movieTitle);
+    } else {
+      // Handle geocoding error
+      console.error('Geocoding failed for movie location');
+    }
+  });
+
+  // ... The rest of your existing code ...
+}
+
+// Initialize the map
+initMap();
+
+// ... Your main JavaScript code here ...
